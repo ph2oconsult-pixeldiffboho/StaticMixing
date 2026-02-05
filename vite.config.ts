cb4e319c7d4f3467258b0fc96a15ac2adde80bd8
ph2oconsult-pixeldiffboho <ph2oconsult@gmail.com>
2026-02-05 12:12:14 +1000

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  }
 });
